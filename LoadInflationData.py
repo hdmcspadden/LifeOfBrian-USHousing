2b34d2743d1d3b25af6d17d189b48a29381a14f8
hdmcspadden <hdmcspadden@gmail.com>
2020-09-30 09:37:58 -0400

    created folder for data discovery. removed loadinflationdata.py

--- LoadInflationData.py
+++
@@ -1,8 +0,0 @@
-# import needed libraries 
-from numpy import *
-import pandas as pd
-
-# testing loading inflation data set
-dfInflation = pd.read_csv('DataSet/inflation_data.csv',header=0,encoding = "ISO-8859-1")
-
-dfInflation.head(5)
