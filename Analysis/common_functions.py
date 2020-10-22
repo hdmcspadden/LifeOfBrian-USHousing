6fa45f494411db8d199b9601deaa3ee8559e9f0e
jhiatt <jordanhiatt@JordansWorkbook.fios-router.home>
2020-10-22 16:08:30 -0400

    gdp evictions part 2

--- Analysis/common_functions.py
+++ Analysis/common_functions.py
@@ -2,27 +2,16 @@
 import pandas as pd
 import sys
 
-# Create a function that adjusts a series of data based on 2019_factor. The function accepts a dataframe, which must have
-#a column labeled 'year' and a column labeled 'value'.
-
-# Function to convert a column labeled 'value', from the passed dataframe to 2019 dollars
-# Years can be from anywhere in the range of 1996-2020
-# Need to instantiate the inflation dataframe for this to work (see above code ) 
+# use this file for any functions used multiple times in different notebooks
 
 def inflation_adjust(df): 
+    # adjust GDP inflation for this specific case only
 
     # Read in the inflation data
     inf_data = pd.read_csv("../dataset/1995inflation.csv")
 
-    # Work with the inflation data to use in an adjustment function
-
-    # Column'amount' is a cumulative price index with 1995 as year 0. 
-    # Calculate a factor for each state that converts 'amount' to 2019 dollars by dividing by 1.68 (the 2019 'amount'value)
-
     # Select the conversion factor from the data frame using .loc
     d2019 = inf_data.loc[inf_data['year'] == 2019,'amount'].item()
-
-    #df = df.rename(columns={'GDP':'value', 'current dollars':'dollar-yr'})
 
     # Apply the factor to all rows
     inf_data['2019_factor']=inf_data['amount']/d2019
