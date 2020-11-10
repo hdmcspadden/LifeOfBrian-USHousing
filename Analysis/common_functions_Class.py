d9cffd8f75bf07c105ab240ee0d6e6c05f3215e3
hdmcspadden <hdmcspadden@gmail.com>
2020-11-10 13:05:39 -0500

    updated common functions unit test

--- Analysis/common_functions_Class.py
+++ Analysis/common_functions_Class.py
@@ -1,4 +1,3 @@
-
 import pandas as pd
 import sys
 
@@ -48,8 +47,27 @@ class COMMONFUNCTIONS:
     # Years can be from anywhere in the range of 1996-2020
     # Need to instantiate the inflation dataframe with following columns:  value	dollar-yr for this to work
     def inflation_adjust_to2019(self, df, price_dict): 
+        # get the inflation dictionary
+        inf_data = self.create_inf_dict(2019)
         
-        df['inf-adjusted'] = round(df['value']/df['dollar-yr'].map(price_dict))
+        df['inf-adjusted'] = round(df['GDP']/df['current dollars'].map(inf_data))
         
         return df
+    
+    def create_inf_dict(self,year):
+        # create a dictionary with inflation conversion factors
+    
+        # Read in the inflation data
+        inf_data = pd.read_csv("../DataSet/1995Inflation.csv")
+    
+        # Select the conversion factor from the data frame using .loc
+        d2019 = inf_data.loc[inf_data['year'] == year,'amount'].item()
+    
+        # Apply the factor to all rows (to get from 1995 factor to 2019 factor)
+        inf_data['2019_factor']=inf_data['amount']/d2019
+    
+        # Store the factors in a dictionary for easy use in a function
+        inf_dict_2019 = dict(zip(inf_data['year'], inf_data['2019_factor']))
+    
+        return inf_dict_2019
 
--- Analysis/common_functions_test.py
+++ Analysis/common_functions_test.py
@@ -14,13 +14,21 @@ class CommonFunctionsTestClass(unittest.TestCase): # inherit from unittest.TestC
         # test GDP dataframe to use for test calculations
         self.dfGDP = pd.DataFrame([[1995, 2001, 1000], [2007, 2010, 1000]], columns=['year', 'current dollars', 'GDP'])
         
-        # dataframe with value and dollar-yr for use in testing inflation_adjust_to2019
-        self.dfDollarsYears = pd.DataFrame([[100,"2017"],[100,"2018"],[100,"2019"]], columns=['value','dollar-yr'])
-        
         # price dictionary with known Constant Multiplier for each year
-        self.price_dict = {"2017":0.95833, "2018":.98214, "2019": 1}
+        self.price_dict = {"1995":0.95833, "2007":.98214}
+        
+    def test_inflation_adjust_to2019(self): 
+        
+        cf = COMMONFUNCTIONS()
+        
+        # run the inflation adject to 2019 dollars function
+        dfAdjusted = cf.inflation_adjust_to2019(self.dfGDP, self.price_dict)
+        
+        # We expect 2017 to be $104, 2018 to be $102, and 2019 to be $100
+        self.assertEqual(str(int(dfAdjusted["inf-adjusted"][0])) + " " + str(int(dfAdjusted["inf-adjusted"][1])), "1448 1175")  
+              
     
-   
+
     def test_correctInflationAdjustmentFrom1995(self):
         # create the COMMONFUNCTIONS class       
         cf = COMMONFUNCTIONS()
@@ -42,16 +50,7 @@ class CommonFunctionsTestClass(unittest.TestCase): # inherit from unittest.TestC
         
         # correct calculation of 2007 dollars entered as 1000 dollars in 2010 dollars is 1175
         self.assertEqual(int(dfInflation["inf-adjusted"][1]), 1175)   
-    
-    def test_inflation_adjust_to2019(self): 
-        
-        cf = COMMONFUNCTIONS()
-        
-        # run the inflation adject to 2019 dollars function
-        dfAdjusted = cf.inflation_adjust_to2019(self.dfDollarsYears, self.price_dict)
-        
-        # We expect 2017 to be $104, 2018 to be $102, and 2019 to be $100
-        self.assertEqual(str(int(dfAdjusted["inf-adjusted"][0])) + " " + str(int(dfAdjusted["inf-adjusted"][1])) + " " + str(int(dfAdjusted["inf-adjusted"][2])), "104 102 100")  
+       
 
 if __name__ == '__main__':
     unittest.main() 
